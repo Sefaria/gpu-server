f8712a2c38354b45818d2ace1c664a89b1999936
nsantacruz <noahssantacruz@gmail.com>
2025-07-24 14:25:32 +0300

    feat: app_helper.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from named_entity_recognizer import NERFactory
-from ne_span import NESpan
+from app_helper import make_recognize_entities_output, make_bulk_recognize_entities_output
 
 
 def create_models_from_config(config):
@@ -14,46 +14,6 @@ def create_models_from_config(config):
         models_by_type_and_lang[model_type][cfg['lang']] = model
 
     return models_by_type_and_lang
-
-
-def get_linker_entities(text, ner_model, ref_part_model):
-    """
-    Extracts named entities and reference parts from the given text using the provided models.
-
-    :param text: The input text to analyze.
-    :param ner_model: The model for named entity recognition.
-    :param ref_part_model: The model for reference part recognition.
-    :return: A tuple containing lists of named entities and reference parts.
-    """
-    spans = ner_model.predict(text)
-    cit_spans, other_spans = [], []
-    for span in spans:
-        if span.label == 'מקור':
-            cit_spans.append(span)
-        else:
-            other_spans.append(span)
-    ref_part_input = [span.text for span in cit_spans]
-    ref_parts = ref_part_model.bulk_predict(ref_part_input, 150)
-    return cit_spans, ref_parts, other_spans
-
-
-def get_serialized_linker_entities(text, ner_model, ref_part_model, with_span_text=False):
-    """
-    Serializes named entities and reference parts from the given text using the provided models.
-
-    :param text: The input text to analyze.
-    :param ner_model: The model for named entity recognition.
-    :param ref_part_model: The model for reference part recognition.
-    :param with_span_text: Whether to include the span text in the serialized output.
-    :return: A list of serialized named entities and ref parts.
-    """
-    cit_spans, ref_parts_list, other_spans = get_linker_entities(text, ner_model, ref_part_model)
-    serial = [span.serialize(with_span_text) for span in other_spans]
-    for span, ref_parts in zip(cit_spans, ref_parts_list):
-        serialized_span = span.serialize(with_span_text)
-        serialized_span['ref_parts'] = [part.serialize(with_span_text) for part in ref_parts]
-        serial.append(serialized_span)
-    return serial
 
 
 def create_app():
@@ -72,7 +32,19 @@ def create_app():
         with_span_text = request.args.get('with_span_text', '0') == '1'
         ner_model = models_by_type_and_lang['named_entity'][data['lang']]
         ref_part_model = models_by_type_and_lang['ref_part'][data['lang']]
-        return jsonify(get_serialized_linker_entities(data['text'], ner_model, ref_part_model, with_span_text)), 200
+        return jsonify(make_recognize_entities_output(data['text'], ner_model, ref_part_model, with_span_text)), 200
+
+    @app.route('/bulk-recognize-entities', methods=['POST'])
+    def bulk_recognize_entities():
+        data = request.get_json(silent=True)
+        if not data or 'texts' not in data:
+            return jsonify({"error": "Missing 'texts' in request body."}), 400
+        with_span_text = request.args.get('with_span_text', '0') == '1'
+        ner_model = models_by_type_and_lang['named_entity'][data['lang']]
+        ref_part_model = models_by_type_and_lang['ref_part'][data['lang']]
+        texts = data['texts']
+        results = make_bulk_recognize_entities_output(texts, ner_model, ref_part_model, with_span_text)
+        return jsonify(results), 200
     return app
 
 
---
+++ app/app_helper.py
@@ -0,0 +1,83 @@
+from functools import reduce
+from collections import defaultdict
+
+
+BATCH_SIZE = 150
+
+
+def make_recognize_entities_output(text, ner_model, ref_part_model, with_span_text=False):
+    cit_spans, ref_parts_list, other_spans = _get_linker_entities(text, ner_model, ref_part_model)
+    return _serialize_linker_entities(cit_spans, ref_parts_list, other_spans, with_span_text)
+
+
+def make_bulk_recognize_entities_output(texts, ner_model, ref_part_model, with_span_text=False):
+    results = _bulk_get_linker_entities(texts, ner_model, ref_part_model)
+    return _bulk_serialize_linker_entities(results, with_span_text)
+
+
+def _get_linker_entities(text, ner_model, ref_part_model):
+    """
+    Extracts named entities and reference parts from the given text using the provided models.
+
+    :param text: The input text to analyze.
+    :param ner_model: The model for named entity recognition.
+    :param ref_part_model: The model for reference part recognition.
+    :return: A tuple containing lists of named entities and reference parts.
+    """
+    spans = ner_model.predict(text)
+    cit_spans, other_spans = _partition_spans(spans)
+    ref_part_input = [span.text for span in cit_spans]
+    ref_parts = ref_part_model.bulk_predict(ref_part_input, BATCH_SIZE)
+    return cit_spans, ref_parts, other_spans
+
+
+def _partition_spans(spans):
+    cit_spans, other_spans = [], []
+    for span in spans:
+        if span.label == 'מקור':
+            cit_spans.append(span)
+        else:
+            other_spans.append(span)
+    return cit_spans, other_spans
+
+
+def _bulk_partition_spans(spans_list):
+    cit_spans_list, other_spans_list = [], []
+    for spans in spans_list:
+        inner_cit_spans, inner_other_spans = _partition_spans(spans)
+        cit_spans_list += [inner_cit_spans]
+        other_spans_list += [inner_other_spans]
+    return cit_spans_list, other_spans_list
+
+
+def _bulk_get_linker_entities(texts, ner_model, ref_part_model):
+    spans_list = ner_model.bulk_predict(texts, BATCH_SIZE)
+    cit_spans_list, other_spans_list = _bulk_partition_spans(spans_list)
+
+    ref_part_input = reduce(lambda a, b: a + [(sub_b.text, b[0]) for sub_b in b[1]], enumerate(cit_spans_list), [])
+    all_raw_ref_part_spans = list(ref_part_model.bulk_predict(ref_part_input, as_tuples=True))
+    all_raw_ref_part_span_map = defaultdict(list)
+    for ref_part_span, input_idx in all_raw_ref_part_spans:
+        all_raw_ref_part_span_map[input_idx] += [ref_part_span]
+
+    output = []
+    for input_idx, cit_spans, other_spans in enumerate(zip(cit_spans_list, other_spans_list)):
+        ref_parts_list = all_raw_ref_part_span_map[input_idx]
+        output += [(cit_spans, ref_parts_list, other_spans)]
+    return output
+
+
+def _serialize_linker_entities(cit_spans, ref_parts_list, other_spans, with_span_text=False):
+    serial = [span.serialize(with_span_text) for span in other_spans]
+    for span, ref_parts in zip(cit_spans, ref_parts_list):
+        serialized_span = span.serialize(with_span_text)
+        serialized_span['ref_parts'] = [part.serialize(with_span_text) for part in ref_parts]
+        serial.append(serialized_span)
+    return serial
+
+
+def _bulk_serialize_linker_entities(results, with_span_text=False):
+    serial = []
+    for cit_spans, ref_parts_list, other_spans in results:
+        serial.append(_serialize_linker_entities(cit_spans, ref_parts_list, other_spans))
+    return serial
