3af99630fcd012969fc74495c9f6ca7e0d03e898
nsantacruz <noahssantacruz@gmail.com>
2025-08-05 14:51:38 +0300

    feat: remove GOOGLE_APPLICATION_CREDENTIALS_FILEPATH from config and update storage client initialization

--- app/config.py
+++ app/config.py
@@ -1,5 +1,4 @@
 class BaseConfig:
-    GOOGLE_APPLICATION_CREDENTIALS_FILEPATH = None
     # Define at most 4 models, one for each language and type
     # 'arch' is the architecture of the model, e.g., 'spacy' or 'huggingface'
     # 'lang' is the language of the model, e.g., 'he' for Hebrew or 'en' for English
--- app/google_storage_manager.py
+++ app/google_storage_manager.py
@@ -9,8 +9,7 @@ class GoogleStorageManager(object):
     def get_bucket(cls, bucket_name):
         if getattr(cls, 'client', None) is None:
             # for local development, change below line to cls.client = storage.Client(project="production-deployment")
-            creds = current_app.config.get('GOOGLE_APPLICATION_CREDENTIALS_FILEPATH')
-            cls.client = storage.Client.from_service_account_json(creds)
+            cls.client = storage.Client.from_service_account_json()
         bucket = cls.client.get_bucket(bucket_name)
         return bucket
 
